f1fffe4fbdfe09d49ba6ed89d527040bb863786f
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-12-21 11:11:47 -0300

    :rocket: Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Exercicio } from './exercicio/entities/exercicio.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { CategoriaModule } from './categoria/categoria.module';
-import { Categoria } from './categoria/entities/categoria.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { UsuarioModule } from './usuario/usuario.module';
+import { ProdService } from './data/services/prod.service';
 import { ExercicioModule } from './exercicio/exercicio.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_fitness',
-      entities: [Exercicio, Categoria, Usuario],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     ExercicioModule,
     CategoriaModule,
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
+import { Exercicio } from '../../exercicio/entities/exercicio.entity';
+import { Usuario } from '../../usuario/entities/usuario.entity';
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_fitness',
+      entities: [Exercicio, Categoria, Usuario],
+      synchronize: true,
+    };
+  }
+}
